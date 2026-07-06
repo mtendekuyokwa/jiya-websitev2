562ddc7c5151f388d23c74dfab8e206ced539a09
mtende kuyokwa <mtendekuyokwa19@gmail.com>
2026-07-07 01:13:06 +0200

    update:adding verifiy in root url

--- app/root.tsx
+++ app/root.tsx
@@ -31,6 +31,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="google-site-verification" content="arHvD01bN1Ev0mYD29wrtKsqlWPyY9giREgXDZ_YH4k" />
         <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
         <link rel="icon" type="image/x-icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
